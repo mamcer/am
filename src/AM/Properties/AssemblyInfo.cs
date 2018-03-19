a7e90ecaff87ee79bc69a32f6410ec8e334d2eeb
Mario Moreno <mamcer@outlook.com>
2018-03-18 22:02:38 -0300

    Add GlobalAssemblyInfo file

--- src/AM/Properties/AssemblyInfo.cs
+++ src/AM/Properties/AssemblyInfo.cs
@@ -1,22 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AM Sound Golem")]
+[assembly: AssemblyTitle("AM.UI")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AM Sound Golem")]
-[assembly: AssemblyCopyright("Copyright © Mario Moreno 2008")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("63537da8-2333-4b06-92bb-9b8656c39b73")]
